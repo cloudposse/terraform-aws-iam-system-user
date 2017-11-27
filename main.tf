b3786104c8e23c7dd3f797b2c010d7a732c8a1fa
Konstantin B <const.bon@gmail.com>
2017-11-27 17:40:54 +0200

    Fix resource count condition (#3)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
@@ -11,7 +11,7 @@ module "label" {
 
 # Defines a user that should be able to write to you test bucket
 resource "aws_iam_user" "default" {
-  count         = "${var.enabled == true ? 1 : 0}"
+  count         = "${var.enabled == "true" ? 1 : 0}"
   name          = "${module.label.id}"
   path          = "${var.path}"
   force_destroy = "${var.force_destroy}"
@@ -19,7 +19,7 @@ resource "aws_iam_user" "default" {
 
 # Generate API credentials
 resource "aws_iam_access_key" "default" {
-  count = "${var.enabled == true ? 1 : 0}"
+  count = "${var.enabled == "true" ? 1 : 0}"
   user  = "${aws_iam_user.default.name}"
 }
 
