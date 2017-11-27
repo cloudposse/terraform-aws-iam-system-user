f1b9a1ba264f242dd15b6881e46e47df07fdcf69
comeanother <const.bon@gmail.com>
2017-11-27 17:28:16 +0200

    Fix

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "label" {
 
 # Defines a user that should be able to write to you test bucket
 resource "aws_iam_user" "default" {
-  count         = "${var.enabled == true ? 1 : 0}"
+  count         = "${var.enabled == "true" ? 1 : 0}"
   name          = "${module.label.id}"
   path          = "${var.path}"
   force_destroy = "${var.force_destroy}"
@@ -19,7 +19,7 @@ resource "aws_iam_user" "default" {
 
 # Generate API credentials
 resource "aws_iam_access_key" "default" {
-  count = "${var.enabled == true ? 1 : 0}"
+  count = "${var.enabled == "true" ? 1 : 0}"
   user  = "${aws_iam_user.default.name}"
 }
 
