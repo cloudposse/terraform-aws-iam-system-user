5cfd58dba904356b765379a92fff92f5a35baaf0
Igor Rodionov <goruha@gmail.com>
2018-03-27 15:54:58 +0600

    Fix warnings

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "user_name" {
-  value = "${aws_iam_user.default.name}"
+  value = "${aws_iam_user.default.*.name}"
 }
 
 output "user_arn" {
-  value = "${aws_iam_user.default.arn}"
+  value = "${aws_iam_user.default.*.arn}"
 }
 
 output "user_unique_id" {
-  value = "${aws_iam_user.default.unique_id}"
+  value = "${aws_iam_user.default.*.unique_id}"
 }
 
 output "access_key_id" {
-  value = "${aws_iam_access_key.default.id}"
+  value = "${aws_iam_access_key.default.*.id}"
 }
 
 output "secret_access_key" {
-  value = "${aws_iam_access_key.default.secret}"
+  value = "${aws_iam_access_key.default.*.secret}"
 }
