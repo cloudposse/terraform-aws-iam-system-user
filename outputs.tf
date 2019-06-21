c686a84b618880a98d61a3b946b3909fb9906079
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:24 +0530

    upgraded-tf

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,26 @@
 output "user_name" {
-  value       = "${join("", aws_iam_user.default.*.name)}"
+  value       = join("", aws_iam_user.default.*.name)
   description = "Normalized IAM user name"
 }
 
 output "user_arn" {
-  value       = "${join("", aws_iam_user.default.*.arn)}"
+  value       = join("", aws_iam_user.default.*.arn)
   description = "The ARN assigned by AWS for this user"
 }
 
 output "user_unique_id" {
-  value       = "${join("", aws_iam_user.default.*.unique_id)}"
+  value       = join("", aws_iam_user.default.*.unique_id)
   description = "The unique ID assigned by AWS"
 }
 
 output "access_key_id" {
-  value       = "${join("", aws_iam_access_key.default.*.id)}"
+  value       = join("", aws_iam_access_key.default.*.id)
   description = "The access key ID"
 }
 
 output "secret_access_key" {
   sensitive   = true
-  value       = "${join("", aws_iam_access_key.default.*.secret)}"
+  value       = join("", aws_iam_access_key.default.*.secret)
   description = "The secret access key. This will be written to the state file in plain-text"
 }
+
--- variables.tf
+++ variables.tf
@@ -11,18 +11,18 @@ variable "stage" {
 }
 
 variable "attributes" {
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
@@ -41,3 +41,4 @@ variable "enabled" {
   description = "Set to false to prevent the module from creating any resources"
   default     = "true"
 }
+
