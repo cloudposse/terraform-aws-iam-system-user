efe3b18f45953913c4ff22fa8b328b648374f478
Andriy Knysh <aknysh@users.noreply.github.com>
2019-06-25 12:06:21 -0400

    Use `required_providers` to pin Terraform providers (#17)
    * Use `required_providers` to pin Terraform providers
    
    * Update `null-label` version. Update example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,13 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
 module "iam_system_user" {
-  source = "../../"
-
-  providers = {
-    aws = "aws"
-  }
-
-  namespace = var.namespace
-  stage     = var.stage
-  name      = var.name
-
+  source        = "../../"
+  namespace     = var.namespace
+  stage         = var.stage
+  name          = var.name
   force_destroy = true
 }
