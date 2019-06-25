5a1b89f0de386ce138ae9d7c56b42816bccc38a5
aknysh <andriy.knysh@gmail.com>
2019-06-25 11:39:24 -0400

    Update `null-label` version. Update example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,15 +3,9 @@ provider "aws" {
 }
 
 module "iam_system_user" {
-  source = "../../"
-
-  providers = {
-    aws = "aws"
-  }
-
-  namespace = var.namespace
-  stage     = var.stage
-  name      = var.name
-
+  source        = "../../"
+  namespace     = var.namespace
+  stage         = var.stage
+  name          = var.name
   force_destroy = true
 }
